9e2b8bac8355560728f2524d263a3d4b405f243f
Raul Dimar <rdimar@shelde.com>
2016-11-04 04:02:01 +0000

    created for v6.1.11; use internal url

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,12 +4,14 @@ $ErrorActionPreference = 'Stop';
 
 
 $packageName = 'splunkforwarder'
-$version = '6.4.2'
-$suffix = '00f5bb3fa822' 
+$version = '6.1.11'
+$suffix = '277527' 
 $registryUninstallerKeyName = 'splunkforwarder' #ensure this is the value in the registry
 $installerType = 'MSI'
-$url =   "https://download.splunk.com/products/universalforwarder/releases/$version/windows/$packageName-$version-$suffix-x86-release.msi"
-$url64 = "https://download.splunk.com/products/universalforwarder/releases/$version/windows/$packageName-$version-$suffix-x64-release.msi"
+$original_url = "https://download.splunk.com/products/universalforwarder/releases/$version/windows"
+$internal_url = 'http://ip-172-31-30-47.ap-southeast-2.compute.internal/splunk'
+$url =   "$internal_url/$packageName-$version-$suffix-x86-release.msi"
+$url64 = "$internal_url/$packageName-$version-$suffix-x64-release.msi"
 $silentArgs = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0)
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -2,9 +2,10 @@
 
 
 $packageName = 'splunkforwarder'
-$registryUninstallerKeyName = '{A3FA04AC-2CF4-4AC5-96A5-1D954080A78E}' #ensure this is the value in the registry
+$registryUninstallerKeyName = '{CFE19569-5C90-46FB-B290-EAF197B03185}' #ensure this is the value in the registry
 $installerType = 'MSI'
 $silentArgs = "$registryUninstallerKeyname /quiet"
 $validExitCodes = @(0)
 
 Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $registryUninstallerKeyName
+# 
